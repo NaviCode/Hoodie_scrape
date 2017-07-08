080706b2ff17f4faf2b0ff3c4a90b7d896df7753
Fernando Sanchez <ext-fernando.sanchez@here.com>
2017-07-07 21:23:39 -0400

    init commit

---
+++ index.rb
@@ -0,0 +1,29 @@
+require 'HTTParty'
+require 'Nokogiri'
+require 'JSON'
+require 'pry'
+require 'csv'
+require 'watir'
+
+$website = "http://btsofficialshop.com/shop/shopdetail.html?branduid=1101244&xcode=004&mcode=009&scode=&type=Y&sort=manual&cur_code=004009&GfDT=Zmt3U1U%3D"
+
+def open_browser
+    browser = Watir::Browser.new :firefox
+    browser.goto $website
+end
+
+while true
+    page = HTTParty.get($website)
+    parse_page = Nokogiri::HTML(page)
+
+    filter_page = parse_page.css('.col-xs-5').text.gsub(/\s+/,"")
+    sold_out = filter_page.include? "Soldout"
+    unless sold_out === false
+        puts "Sold out @ #{Time.now}"
+    else
+        open_browser
+        break
+    end
+end
+
+
